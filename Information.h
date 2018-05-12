f2e3b3536140cc0a6a13c429db562b3c64466bc1
947350632 <38323015+947350632@users.noreply.github.com>
2018-05-12 23:59:12 +0800

    Add files via upload

---
+++ Information.h
@@ -0,0 +1,50 @@
+//主菜单
+void menu(void);
+
+//添加信息
+void ID(void);
+
+//修改信息
+void change(void);
+
+//按手机号码修改 
+void phonechange(void);
+
+//按姓名修改 
+void namechange(void);
+
+//查询信息
+void search(void); 
+
+//按手机号码查询
+void phonesearch(void);
+
+//按姓名查询 
+void namesearch(void); 
+
+//全部显示
+void show(void);   
+
+//删除信息
+void del(void);  
+
+//按手机号码删除
+void phonedel(void);
+
+//按姓名删除 
+void namedel(void);
+
+//信息排序
+void order(void);      
+
+//按照会员卡号排序 
+void idorder(void); 
+
+//按照总分排序
+void scoreorder(void);  
+
+//保存数据
+void filewrite(void);	
+
+//读取数据
+void fileread(void);	
